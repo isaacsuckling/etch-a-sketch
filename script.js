5ec112cedcf622d0190eecc5e7e1f0f179e8f3cb
Isaac Suckling <isaacsuckling03@gmail.com>
2025-07-28 14:06:57 +1000

    finishing game

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 const container = document.querySelector('.container');
+const promtButton = document.querySelector('.promt-button');
 
-for(let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+
+function createGrid(size) {
+    container.innerHTML = '';
+    const squareSize = 960 / size;
+    
+    for(let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'red';
+        });
+
+        container.appendChild(square);
+    }
+
 }
+
+
+promtButton.addEventListener('click', () => {
+    let size = prompt('How many squares per side? (max 100)');
+    size = parseInt(size);
+
+    if (isNaN(size) || size < 1 || size > 100) {
+        alert('Please enter a number between 1 and 100.')
+        return;
+    }
+
+    createGrid(size);
+});
+
+
+// Default Grid
+createGrid(16);
